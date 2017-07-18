b0bdfc8dd6b8d7b064c7eedf76dba45aad9bd3c1
Chris Sangwin <C.J.Sangwin@ed.ac.uk>
2017-07-18 13:48:04 +0100

    Minor changes to the install docs for Debian.

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 wd=$(dirname "$0")
+
 xelatex=$(which xelatex)
+if [$xelatex == ""]
+then
+   xelatex=$(which latex)
+fi
 
 stack exec robotone-exe > "$wd/build/robotone.tex"
 echo "TeX"
